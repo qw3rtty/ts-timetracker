4ba4264908fd23df8ad23afbee4febcdc92da736
Thomas Schwarz <thomasschwarz96@web.de>
2019-11-16 18:24:33 +0100

    Created application structure

---
+++ controllers/TS_Base.cpp
@@ -0,0 +1,21 @@
+/**
+ * TS-TimeTracker
+ *
+ * Base class for whole application.
+ *
+ * @package     TS-TimeTracker
+ * @author      Thomas Schwarz
+ * @copyright   Copyright (c) 2019, Thomas Schwarz
+ * @license     MIT License (https://opensource.org/licenses/MIT)
+ * @since       Version 0.1.0
+ * @filesource
+ */
+
+#include <stdio.h>
+#include "./headers/TS_Base.h"
+
+/**
+ * Constructor
+ */
+TS_Base::TS_Base()
+{}
---
+++ controllers/headers/TS_Base.h
@@ -0,0 +1,26 @@
+#ifndef TS_TIMETRACKER_TS_BASE_H
+#define TS_TIMETRACKER_TS_BASE_H
+
+/**
+ * TS-TimeTracker
+ *
+ * Base class for whole application.
+ *
+ * @package     TS-TimeTracker
+ * @author      Thomas Schwarz
+ * @copyright   Copyright (c) 2019, Thomas Schwarz
+ * @license     MIT License (https://opensource.org/licenses/MIT)
+ * @since       Version 0.1.0
+ * @filesource
+ */
+
+#include <stdio.h>
+
+class TS_Base
+{
+public:
+    TS_Base();
+    void test();
+};
+
+#endif //TS_TIMETRACKER_TS_BASE_H
