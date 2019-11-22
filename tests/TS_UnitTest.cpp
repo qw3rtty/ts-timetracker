de0608c0e42e11b7c9a6750230bbda0597173f4b
Thomas Schwarz <thomasschwarz96@web.de>
2019-11-22 20:42:40 +0100

    Formatted Unit test output

--- tests/TS_UnitTest.cpp
+++ tests/TS_UnitTest.cpp
@@ -16,6 +16,7 @@
 #include <cstring>
 #include <cassert>
 
+#include "../helpers/headers/TS_Helper.h"
 #include "../application/controllers/headers/TS_Base.h"
 #include "../application/controllers/headers/TS_CommandDelegator.h"
 #include "../application/controllers/headers/TS_Command.h"
@@ -23,19 +24,21 @@
 
 int main()
 {
+    TS_Helper helper;
+
     std::cout << "Unit tests starting..." << std::endl;
 
     assert(commandDelegator->unitTest());
-    std::cout << "TS_CommandDelegator passed " << " \u2713" << std::endl;
+    std::cout << "TS_CommandDelegator passed " << helper.color << " \u2713" << helper.noColor << std::endl;
 
     TS_Command cmd;
     assert(!cmd.execute());
-    std::cout << "TS_Command passed " << " \u2713" << std::endl;
+    std::cout << "TS_Command passed " << helper.color << " \u2713" << helper.noColor << std::endl;
 
     TS_CommandHelp cmdHelp;
     assert(cmdHelp.execute());
-    std::cout << "TS_CommandHelp passed " << " \u2713" << std::endl;
+    std::cout << "TS_CommandHelp passed " << helper.color << " \u2713" << helper.noColor << std::endl;
 
-    std::cout << "All tests passed" << " \u2713" << std::endl;
+    std::cout << "All tests passed" << helper.color << " \u2713" << helper.noColor << std::endl;
     return 0;
 }
