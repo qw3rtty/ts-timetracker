6447aefdad965e9fd439a4d687abfdabaf7a75a1
Thomas Schwarz <thomasschwarz96@web.de>
2020-01-14 00:12:41 +0100

    Update unit tests

--- tests/TS_UnitTest.cpp
+++ tests/TS_UnitTest.cpp
@@ -18,11 +18,16 @@
 #include "TS_Helper.h"
 #include "TS_Info.h"
 #include "TS_Application.h"
+#include "TS_CommandExport.h"
 #include "TS_CommandHelp.h"
 #include "TS_CommandList.h"
 #include "TS_CommandRemove.h"
+#include "TS_CommandSave.h"
 #include "TS_CommandTimestamp.h"
+#include "TS_CommandTimestampStart.h"
+#include "TS_CommandTimestampStop.h"
 #include "TS_CommandTouch.h"
+#include "TS_CommandUse.h"
 
 int main()
 {
@@ -46,6 +51,11 @@ int main()
     assert(!cmd.execute());
     std::cout << "TS_Command passed " << helper.color << " \u2713" << helper.noColor << std::endl;
 
+    TS_CommandExport cmdExport(testArgument);
+    cmdExport.setTestMode(true);
+    assert(cmdExport.execute());
+    std::cout << "TS_CommandExport passed " << helper.color << " \u2713" << helper.noColor << std::endl;
+
     TS_CommandHelp cmdHelp(testArgument);
     cmdHelp.setTestMode(true);
     assert(cmdHelp.execute());
@@ -61,15 +71,35 @@ int main()
     assert(cmdRemove.execute());
     std::cout << "TS_CommandRemove passed " << helper.color << " \u2713" << helper.noColor << std::endl;
 
+    TS_CommandSave cmdSave(testArgument);
+    cmdSave.setTestMode(true);
+    assert(cmdSave.execute());
+    std::cout << "TS_CommandSave passed " << helper.color << " \u2713" << helper.noColor << std::endl;
+
     TS_CommandTimestamp cmdTime(testArgument);
     assert(cmdTime.execute());
     assert(cmdTime.getTimestamp() != -1);
     std::cout << "TS_CommandTimestamp passed " << helper.color << " \u2713" << helper.noColor << std::endl;
 
+    TS_CommandTimestampStart cmdTimeStart(testArgument);
+    assert(cmdTimeStart.execute());
+    assert(cmdTimeStart.getTimestamp() != -1);
+    std::cout << "TS_CommandTimestampStart passed " << helper.color << " \u2713" << helper.noColor << std::endl;
+
+    TS_CommandTimestampStop cmdTimeStop(testArgument);
+    assert(cmdTimeStop.execute());
+    assert(cmdTimeStop.getTimestamp() != -1);
+    std::cout << "TS_CommandTimestampStop passed " << helper.color << " \u2713" << helper.noColor << std::endl;
+
     TS_CommandTouch cmdTouch(testArgument);
     cmdTouch.setTestMode(true);
     assert(cmdTouch.execute());
     std::cout << "TS_CommandTouch passed " << helper.color << " \u2713" << helper.noColor << std::endl;
+
+    TS_CommandUse cmdUse(testArgument);
+    cmdUse.setTestMode(true);
+    assert(cmdUse.execute());
+    std::cout << "TS_CommandUse passed " << helper.color << " \u2713" << helper.noColor << std::endl;
 
     std::cout << std::endl;
     std::cout << helper.color << "All tests passed" << " \u2713" << helper.noColor << std::endl;
