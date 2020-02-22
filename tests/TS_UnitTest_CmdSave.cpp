eedc6e156b0470f06d64675e8e1cfcabd7cabe67
Thomas Schwarz <thomasschwarz96@web.de>
2020-02-22 17:48:17 +0100

    Add unit test for command save

---
+++ tests/TS_UnitTest_CmdSave.cpp
@@ -0,0 +1,29 @@
+/**
+ * TS-UnitTest Command Save
+ *
+ * Unit test for the Save command.
+ *
+ * @package     TS-TimeTracker
+ * @author      Thomas Schwarz
+ * @copyright   Copyright (c) 2020, Thomas Schwarz
+ * @license     MIT License (https://opensource.org/licenses/MIT)
+ * @since       Version 0.1.0
+ * @filesource
+ */
+
+#include <iostream>
+#include <cassert>
+
+#include "TS_Helper.h"
+#include "TS_CommandSave.h"
+
+int main()
+{
+    TS_CommandSave* cmd = new TS_CommandSave(nullptr);
+    cmd->setTestMode(true);
+
+    assert(cmd->execute());
+    std::cout << "TS_CommandSave passed" << " \u2713" << std::endl;
+
+    return 0;
+}
