cab8ab6b2635af050c931217c7ecdb042e744ba1
Thomas Schwarz <thomasschwarz96@web.de>
2020-02-22 18:06:27 +0100

    Add unit test for filesystem model

---
+++ tests/TS_UnitTest_ModelFilesystem.cpp
@@ -0,0 +1,39 @@
+/**
+ * TS-UnitTest Model Filesystem
+ *
+ * Unit test for the filesystem model.
+ *
+ * @package     TS-TimeTracker
+ * @author      Thomas Schwarz
+ * @copyright   Copyright (c) 2020, Thomas Schwarz
+ * @license     MIT License (https://opensource.org/licenses/MIT)
+ * @since       Version 0.1.0
+ * @filesource
+ */
+
+#include <iostream>
+#include <cassert>
+
+#include <string>
+#include <map>
+#include <iterator>
+#include <cstring>
+#include <fstream>
+#include <dirent.h>
+#include <sstream>
+
+#include "TS_Helper.h"
+#include "TS_ConfigReader.h"
+#include "TS_ModelFilesystem.h"
+
+int main()
+{
+    TS_ModelFilesystem* model = new TS_ModelFilesystem();
+    model->setProject(0);
+
+    assert(model->getName() != "");
+
+    std::cout << "TS_ModelFilesystem passed" << " \u2713" << std::endl;
+
+    return 0;
+}
